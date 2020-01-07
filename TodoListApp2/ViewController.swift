14cfe66113c33277806c470ea6c74c2f417aa3c7
crowjambo <evaldascrow@gmail.com>
2020-01-07 12:53:04 +0200

    Initial Commit

---
+++ TodoListApp2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TodoListApp2
+//
+//  Created by Evaldas on 1/7/20.
+//  Copyright © 2020 Evaldas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
---
+++ TodoListApp2Tests/TodoListApp2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  TodoListApp2Tests.swift
+//  TodoListApp2Tests
+//
+//  Created by Evaldas on 1/7/20.
+//  Copyright © 2020 Evaldas. All rights reserved.
+//
+
+import XCTest
+@testable import TodoListApp2
+
+class TodoListApp2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
