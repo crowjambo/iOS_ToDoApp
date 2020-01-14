b127f7e8d5647b83b1243055d613d30740f0e6cd
crowjambo <evaldascrow@gmail.com>
2020-01-14 11:39:07 +0200

    UI tests for add/delete/edit task

--- TodoListApp2UITests/TodoListApp2UITests.swift
+++ TodoListApp2UITests/TodoListApp2UITests.swift
@@ -22,15 +22,105 @@ class TodoListApp2UITests: XCTestCase {
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
+	
+	
+	func test_adding_new_task(){
+		let app = XCUIApplication()
+		app.launch()
+		
+		let itemName = "some name"
+		let itemDesc = "some description"
 
-    func testExample() {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
+		app.navigationBars["TodoListApp2.View"].buttons["Add"].tap()
+		
+		let addNameTextField = app.textFields["Add Name"]
+		addNameTextField.tap()
+		addNameTextField.typeText(itemName)
+		
+		let addDescriptionTextField = app.textFields["Add Description"]
+		addDescriptionTextField.tap()
+		addDescriptionTextField.typeText(itemDesc)
+		
+		let addButton = app.buttons["Add"]
+		addButton.tap()
 
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+		let newItem = app.tables.staticTexts[itemName]
+		
+		
+		XCTAssertTrue(newItem.exists)
+	}
+	
+	func test_removing_task(){
+		
+		let app = XCUIApplication()
+		app.launch()
+		
+		// adding new item first
+		let itemName = "some name"
+		let itemDesc = "some description"
+
+		app.navigationBars["TodoListApp2.View"].buttons["Add"].tap()
+		
+		let addNameTextField = app.textFields["Add Name"]
+		addNameTextField.tap()
+		addNameTextField.typeText(itemName)
+		
+		let addDescriptionTextField = app.textFields["Add Description"]
+		addDescriptionTextField.tap()
+		addDescriptionTextField.typeText(itemDesc)
+		
+		let addButton = app.buttons["Add"]
+		addButton.tap()
+
+		
+		// removing with a swipe
+		let newItem = app.tables.staticTexts[itemDesc]
+		newItem.swipeLeft()
+		app.tables/*@START_MENU_TOKEN@*/.buttons["trailing0"]/*[[".cells",".buttons[\"Delete\"]",".buttons[\"trailing0\"]"],[[[-1,2],[-1,1],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/.tap()
+		
+		XCTAssertFalse(newItem.exists)
+		
+	}
+	
+	func test_editing_task(){
+		let app = XCUIApplication()
+		app.launch()
+		
+		// adding new item first
+		let itemName = "some name"
+		let itemDesc = "some description"
+
+		app.navigationBars["TodoListApp2.View"].buttons["Add"].tap()
+		
+		let addNameTextField = app.textFields["Add Name"]
+		addNameTextField.tap()
+		addNameTextField.typeText(itemName)
+		
+		let addDescriptionTextField = app.textFields["Add Description"]
+		addDescriptionTextField.tap()
+		addDescriptionTextField.typeText(itemDesc)
+		
+		let addButton = app.buttons["Add"]
+		addButton.tap()
+		
+		// editing
+		let newItem = app.tables.staticTexts[itemDesc]
+		newItem.tap()
+		let newNameAddition = "1"
+		let newDescAddition = "1"
+		addNameTextField.tap()
+		addNameTextField.typeText(newNameAddition)
+		
+		addDescriptionTextField.tap()
+		addDescriptionTextField.typeText(newDescAddition)
+		let updateButton = app.buttons["Update"]
+		updateButton.tap()
+		
+		//final check
+		let newName = itemName+newNameAddition
+		let editedItem = app.tables.staticTexts[newName]
+		XCTAssertTrue(editedItem.exists)
+	}
 
     func testLaunchPerformance() {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
